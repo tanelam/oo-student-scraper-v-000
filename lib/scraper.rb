91da78ee7dd239a86c7c2cb4dbd0b2d384a06ab1
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-11-07 02:35:53 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ class Scraper
   def self.scrape_index_page(index_url)
      doc = Nokogiri::HTML(open(index_url))
      students = []
-     doc.css("div.roster-cards-container").each do |student_card|
+    #  doc.css("div.roster-cards-container").each do |student_card|
        doc.css("div.student-card").each do |card|
          student_name = card.css(".student-name").text
          student_location = card.css(".student-location").text
