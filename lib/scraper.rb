a7fc18334a5d1705b8e8cf8e5c4a050474aeb1f9
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-11-07 00:58:00 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,28 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+     doc = Nokogiri::HTML(open(index_url))
+     students = doc.css("div.roster-cards-container").text
+     student_card = doc.css (".student-card").text
+            <a href="students/ryan-johnson.html">
+              <div class="view-profile-div">
+                <h3 class="view-profile-text">View Profile</h3>
+              </div>
+              <div class="card-text-container">
+                <h4 class="student-name">Ryan Johnson</h4>
+                <p class="student-location">New York, NY</p>
+              </div>
+            </a>
+          </div>)
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
