4b8ad48276ed7d3596a1d1a7993b080d9bf7ec26
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-11-07 03:05:09 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,7 +19,8 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-    doc = Nokogiri::HTML(open(index_url))
+    doc = Nokogiri::HTML(open(profile_url))
+    student = {}
 
 
   end
